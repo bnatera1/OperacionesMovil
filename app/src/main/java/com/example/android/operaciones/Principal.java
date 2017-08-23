37ccfa8edb977f92101287f574fefa013810f2f8
Breiner <breiner@natera.com.co>
2017-08-22 20:03:20 -0500

    Interfaz operaciones terminadas

--- app/src/main/java/com/example/android/operaciones/Principal.java
+++ app/src/main/java/com/example/android/operaciones/Principal.java
@@ -4,7 +4,9 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class Principal extends AppCompatActivity {
     private TextView res;
     private EditText n1, n2;
     private Resources resources;
+    private Spinner operaciones;
+    private String op[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +25,21 @@ public class Principal extends AppCompatActivity {
         res = (TextView)findViewById(R.id.lblResultado);
         n1 = (EditText)findViewById(R.id.txtNumeroUno);
         n2 = (EditText)findViewById(R.id.txtNumeroDos);
+        operaciones =(Spinner)findViewById(R.id.cmbOperaciones);
         resources = this.getResources();
+        op = resources.getStringArray(R.array.operaciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,op);
+        operaciones.setAdapter(adapter);
 
     }
 
     public boolean validar() {
         if (n1.getText().toString().isEmpty()) {
-            n1.setError(resources.getString(R.string.mensaje_error_uno));
+            n1.setError(resources.getString(R.string.msm_error_uno));
             return false;
         }
         if (n2.getText().toString().isEmpty()) {
-            n2.setError(resources.getString(R.string.mesaje_error_dos));
+            n2.setError(resources.getString(R.string.msm_error_dos));
             return false;
         }
         return true;
